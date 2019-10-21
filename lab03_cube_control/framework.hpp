52300a0501c671a9244c5e3c842a6b5b34b02c3f
Yukino Hayakawa <tennencoll@gmail.com>
2019-10-21 10:52:09 +0800

    code for lab 3

---
+++ lab03_cube_control/framework.hpp
@@ -0,0 +1,257 @@
+﻿#pragma once
+
+#ifdef _WIN32
+#   pragma comment(lib, "OpenGL32.lib")
+#   define WIN32_LEAN_AND_MEAN
+#   include <Windows.h>
+#   undef near
+#   undef far
+#   undef max
+#   undef min
+#   include <GL/gl.h>
+#   include <GL/glu.h>
+#endif
+
+#ifdef __APPLE__
+#   define GL_SILENCE_DEPRECATION
+#   include <OpenGL/gl.h>
+#   include <OpenGL/glu.h>
+#endif
+
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+/*****************************************************************************/
+// Object
+/*****************************************************************************/
+
+class Object
+{
+protected:
+    /**
+     * \brief Origin of the object in Cartesian coordinates.
+     */
+    glm::vec3 mPosition = { 0, 0, 0 };
+
+    /**
+     * \brief Orientation of the object using Euler angles.
+     */
+    glm::vec3 mOrientation = { 0, 0, 0 };
+
+    /**
+     * \brief Scaling of the object on each axis.
+     */
+    glm::vec3 mScaling = { 1, 1, 1 };
+
+public:
+    virtual ~Object() = default;
+
+    glm::vec3 & position() { return mPosition; }
+    glm::vec3 & orientation() { return mOrientation; }
+    glm::vec3 & scaling() { return mScaling; }
+
+    void setTransformation(
+        const glm::vec3 &position = { 0, 0, 0 },
+        const glm::vec3 &orientation = { 0, 0, 0 },
+        const glm::vec3 &scaling = { 1, 1, 1, })
+    {
+        mPosition = position;
+        mOrientation = orientation;
+        mScaling = scaling;
+    }
+
+    /**
+     * \brief Concatenate the matrix for converting positions into the local
+     * space of this object. Useful for cameras.
+     */
+    void applyWorldToLocal() const
+    {
+        // keep in mind how the matrix stack & transformation works
+        glScalef(1 / mScaling.x, 1 / mScaling.y, 1 / mScaling.z);
+        glRotatef(-mOrientation.x, 1, 0, 0);
+        glRotatef(-mOrientation.y, 0, 1, 0);
+        glRotatef(-mOrientation.z, 0, 0, 1);
+        glTranslatef(-mPosition.x, -mPosition.y, -mPosition.z);
+    }
+
+    /**
+     * \brief Concatenate the matrix for converting positions from the local
+     * space of the object into the world space. Useful for renderable objects.
+     */
+    void applyLocalToWorld() const
+    {
+        glTranslatef(mPosition.x, mPosition.y, mPosition.z);
+        glRotatef(mOrientation.z, 0, 0, 1);
+        glRotatef(mOrientation.y, 0, 1, 0);
+        glRotatef(mOrientation.x, 1, 0, 0);
+        glScalef(mScaling.x, mScaling.y, mScaling.z);
+    }
+
+    /**
+     * \brief Issue drawing commands.
+     * \param dt
+     */
+    virtual void draw(float dt) = 0;
+
+    void drawTransformed(float dt)
+    {
+        // Save the current matrix
+        glPushMatrix();
+        // Apply cube local-to-world transformation
+        applyLocalToWorld();
+        draw(dt);
+        // Restore to last saved matrix
+        glPopMatrix();
+    }
+
+    /**
+     * \brief Optional methods for updating your object with time.
+     * \param dt
+     */
+    virtual void update(float dt)
+    {
+    }
+};
+
+/*****************************************************************************/
+// Camera
+/*****************************************************************************/
+
+class Camera : public Object
+{
+public:
+    void draw(float dt) override
+    {
+        // Usually we don't draw the camera itself.
+    }
+
+    /**
+     * \brief Apply the projection matrix.
+     */
+    virtual void applyProjection() const = 0;
+};
+
+/*****************************************************************************/
+// PerspectiveCamera
+/*****************************************************************************/
+
+class PerspectiveCamera : public Camera
+{
+    float mFov = 60;
+    float mAspect = 16 / 9.f;
+    float mZNear = 0.5f;
+    float mZFar = 100;
+
+public:
+    PerspectiveCamera() = default;
+
+    PerspectiveCamera(float fov, float aspect, float near, float far)
+        : mFov(fov)
+        , mAspect(aspect)
+        , mZNear(near)
+        , mZFar(far)
+    {
+    }
+
+    void setFov(float fov) { mFov = fov; }
+    void setAspect(float aspect) { mAspect = aspect; }
+    void setZNear(float near) { mZNear = near; }
+    void setZFar(float far) { mZFar = far; }
+
+    void applyProjection() const override
+    {
+        gluPerspective(mFov, mAspect, mZNear, mZFar);
+    }
+};
+
+/******************************************************************************/
+// OrthogonalCamera
+/******************************************************************************/
+
+class OrthogonalCamera : public Camera
+{
+    float mLeft = 0;
+    float mRight = 1280;
+    float mTop = 0;
+    float mBottom = 720;
+    float mNear = -10;
+    float mFar = 10;
+
+public:
+    OrthogonalCamera() = default;
+
+    OrthogonalCamera(
+        float left,
+        float right,
+        float top,
+        float bottom,
+        float near,
+        float far)
+        : mLeft(left)
+        , mRight(right)
+        , mTop(top)
+        , mBottom(bottom)
+        , mNear(near)
+        , mFar(far)
+    {
+    }
+
+    void setLeft(float left) { mLeft = left; }
+    void setRight(float right) { mRight = right; }
+    void setTop(float top) { mTop = top; }
+    void setBottom(float bottom) { mBottom = bottom; }
+    void setNear(float near) { mNear = near; }
+    void setFar(float far) { mFar = far; }
+
+    void applyProjection() const override
+    {
+        glOrtho(mLeft, mRight, mBottom, mTop, mNear, mFar);
+    }
+};
+
+
+void installCallbacks(GLFWwindow* window);
+void initScene();
+void render(float dt);
+void update(float dt);
+void mainLoop(GLFWwindow* window);
+
+extern int gFramebufferWidth;
+extern int gFramebufferHeight;
+extern const char *gWindowTitle;
+
+int main(void)
+{
+    GLFWwindow *window = nullptr;
+
+    // Initialize the library
+    if(!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(
+        gFramebufferWidth, gFramebufferHeight, gWindowTitle,
+        nullptr, nullptr);
+    // If you cannot create the window, update your graphics driver first!
+    if(!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    installCallbacks(window);
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+    // Enable vertical sync
+    glfwSwapInterval(1);
+
+    initScene();
+    mainLoop(window);
+
+    glfwTerminate();
+    return 0;
+}
---
+++ lab03_cube_control/lab03_cube_control.cpp
@@ -0,0 +1,247 @@
+#include "framework.hpp"
+
+/*****************************************************************************/
+// Axis
+/*****************************************************************************/
+
+class Axis : public Object
+{
+    float mSize = 0.1f;
+
+public:
+    Axis() = default;
+
+    Axis(float size)
+        : mSize(size)
+    {
+    }
+
+    void setSize(float size) { mSize = size; }
+
+    void draw(float dt) override
+    {
+        glBegin(GL_LINES);
+            // x-axis
+            glColor3f(1, 0, 0);
+            glVertex3f(0, 0, 0);
+            glVertex3f(1, 0, 0);
+            // y-axis
+            glColor3f(0, 1, 0);
+            glVertex3f(0, 0, 0);
+            glVertex3f(0, 1, 0);
+            // z-axis
+            glColor3f(0, 0, 1);
+            glVertex3f(0, 0, 0);
+            glVertex3f(0, 0, 1);
+        glEnd();
+    }
+};
+
+
+/*****************************************************************************/
+// MeshGround
+/*****************************************************************************/
+
+class MeshGround : public Object
+{
+    float mSize = 50.f;
+    float mStep = 5.f;
+    float mHeight = -2.5f;
+
+public:
+    MeshGround() = default;
+
+    MeshGround(float size, float step, float height)
+        : mSize(size)
+        , mStep(step)
+        , mHeight(height)
+    {
+    }
+
+    void setSize(float size) { mSize = size; }
+    void setStep(float step) { mStep = step; }
+    void setHeight(float height) { mHeight = height; }
+
+    void draw(float dt) override
+    {
+        glBegin(GL_LINES);
+            glColor3f(0.75f, 0.75f, 0.75f);
+            // draw horizontal lines
+            for(float i = -mSize; i < mSize; i += mStep)
+            {
+                glVertex3f(-mSize, mHeight, i);
+                glVertex3f(mSize, mHeight, i);
+            }
+            // draw deep lines
+            for(float i = -mSize; i < mSize; i += mStep)
+            {
+                glVertex3f(i, mHeight, -mSize);
+                glVertex3f(i, mHeight, mSize);
+            }
+        glEnd();
+    }
+};
+
+/*****************************************************************************/
+// Cube
+/*****************************************************************************/
+
+#define QUAD_INDICES(array, a, b, c, d) \
+    glVertex3fv(value_ptr(array[a])); \
+    glVertex3fv(value_ptr(array[b])); \
+    glVertex3fv(value_ptr(array[c])); \
+    glVertex3fv(value_ptr(array[d])); \
+/**/
+
+class Cube : public Object
+{
+    float mHalfSize = 0.5f;
+
+public:
+    Cube() = default;
+
+    Cube(float half_size)
+        : mHalfSize(half_size)
+    {
+    }
+
+    void setHalfSize(float half_size) { mHalfSize = half_size; }
+
+    void draw(float dt) override
+    {
+        const glm::vec3 v[] = {
+            { mHalfSize, mHalfSize, mHalfSize },
+            { mHalfSize, mHalfSize, -mHalfSize },
+            { mHalfSize, -mHalfSize, mHalfSize },
+            { mHalfSize, -mHalfSize, -mHalfSize },
+            { -mHalfSize, mHalfSize, mHalfSize },
+            { -mHalfSize, mHalfSize, -mHalfSize },
+            { -mHalfSize, -mHalfSize, mHalfSize },
+            { -mHalfSize, -mHalfSize, -mHalfSize },
+        };
+
+        glBegin(GL_QUADS);
+        // top
+        glColor3ub(169, 102, 194);
+        QUAD_INDICES(v, 1, 0, 4, 5);
+        // bottom
+        glColor3ub(164, 33, 14);
+        QUAD_INDICES(v, 2, 3, 7, 6);
+        // left
+        glColor3ub(228, 69, 147);
+        QUAD_INDICES(v, 5, 4, 6, 7);
+        // right
+        glColor3ub(136, 157, 210);
+        QUAD_INDICES(v, 0, 1, 3, 2);
+        // front
+        glColor3ub(138, 250, 122);
+        QUAD_INDICES(v, 1, 5, 7, 3);
+        // back
+        glColor3ub(1, 37, 146);
+        QUAD_INDICES(v, 4, 0, 2, 6);
+        glEnd();
+    }
+};
+
+/*****************************************************************************/
+// Game Logics
+/*****************************************************************************/
+
+int gFramebufferWidth = 1280;
+int gFramebufferHeight = 720;
+const char *gWindowTitle = "Cube";
+
+PerspectiveCamera gCamera;
+Cube gCube;
+Axis gAxis;
+MeshGround gGround;
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    gFramebufferWidth = width;
+    gFramebufferHeight = height;
+    gCamera.setAspect(
+        static_cast<float>(gFramebufferWidth) / gFramebufferHeight
+    );
+}
+
+double gLastMouseX = 0;
+double gLastMouseY = 0;
+
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    auto dx = xpos - gLastMouseX;
+    auto dy = ypos - gLastMouseY;
+    gLastMouseX = xpos;
+    gLastMouseY = ypos;
+    if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_2))
+        gCamera.orientation().y -= (float)dx * 0.1f;
+}
+
+void initScene()
+{
+    // TODO: set the positions of your cube and camera
+
+    // I already put the camera at a good position. You only have to
+    // move the cube.
+    gCamera.position().z = 5;
+}
+
+void update(float dt)
+{
+    gCube.orientation().y += dt * 10;
+}
+
+void render(float dt)
+{
+    glViewport(0, 0, gFramebufferWidth, gFramebufferHeight);
+    glEnable(GL_DEPTH_TEST);
+
+    glMatrixMode(GL_PROJECTION);
+    // Reset the matrix
+    glLoadIdentity();
+    // Apply projection matrix
+    gCamera.applyProjection();
+
+    glMatrixMode(GL_MODELVIEW);
+    // Reset the matrix
+    glLoadIdentity();
+    // Apply camera world-to-local transformation
+    gCamera.applyWorldToLocal();
+    // This is a new function which applies the transformation automatically
+    gAxis.drawTransformed(dt);
+    gGround.drawTransformed(dt);
+    // gCube.drawTransformed(dt);
+}
+
+void installCallbacks(GLFWwindow *window)
+{
+    // Register our callbacks so we can know when the events occur.
+    glfwSetCursorPosCallback(window, cursor_position_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+}
+
+void mainLoop(GLFWwindow *window)
+{
+    auto time = glfwGetTime();
+
+    // Loop until the user closes the window
+    while(!glfwWindowShouldClose(window))
+    {
+        const auto new_time = glfwGetTime();
+        // Elapsed time since last frame, in seconds.
+        const auto dt = new_time - time;
+        time = new_time;
+
+        update(static_cast<float>(dt));
+        // Clear the framebuffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // Set up the camera and draw our scene
+        render(static_cast<float>(dt));
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+        // Poll for and process events, this will call into the callbacks
+        // we set before when related events occur.
+        glfwPollEvents();
+    }
+}
